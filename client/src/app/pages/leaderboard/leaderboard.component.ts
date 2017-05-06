e3fb88257e21f760e7c7120384ad50c9e5207af7
cyllo <kurthogarth@gmail.com>
2017-05-06 13:31:28 -0700

    Update
    - Body for a post now at minimum takes up 100% of the viewport height.
    - Base for following page added
    - Basic styling applied to heroes page
    - Leaderboard base added

--- client/src/app/pages/leaderboard/leaderboard.component.ts
+++ client/src/app/pages/leaderboard/leaderboard.component.ts
@@ -5,14 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'leaderboard.component.html',
   styleUrls: ['leaderboard.component.scss']
 })
+
 export class LeaderboardComponent implements OnInit {
+
+  private columns;
+  private rows;
 
   constructor() {
     // Do stuff
   }
 
   ngOnInit() {
+
     console.log('Hello leaderboard');
+
+    this.columns = [
+      { prop : 'player' },
+      { name : 'skillrating' }
+    ];
+
+    this.rows = [
+      { player: 'Cyllo' , skillrating: '5000'}
+    ];
+
   }
 
 }
