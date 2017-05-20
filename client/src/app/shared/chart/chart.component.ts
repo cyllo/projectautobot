86f2fd3cce876147a2e87795a3e0fb37c3d87c06
cyllo <kurthogarth@gmail.com>
2017-05-20 14:27:35 -0700

    updated chart component options

--- client/src/app/shared/chart/chart.component.ts
+++ client/src/app/shared/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'ow-chart',
@@ -7,7 +7,7 @@ import { Component, ViewChild } from '@angular/core';
 })
 
 export class ChartComponent {
-  @ViewChild('owChart') owChart;
+  @ViewChild('owChart') owChart: ElementRef;
 
   //  6 types of charts: line, bar, radar, pie, polarArea, doughnut
   public chartType = 'line';
@@ -23,14 +23,49 @@ export class ChartComponent {
   //   {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
   //   {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
   // ];
-  public chartData: Array<any> = [
-    { data: [2300, 2818, 1920, 3498, 4200, 4220, 4786], label: 'Series A' },
-    { data: [1300, 1818, 2400, 1992, 3800, 3890, 4200], label: 'Series B' }
+  public chartDatasets: Array<any> = [
+    {data: [2300, 2818, 1920, 3498, 4200, 4220, 4786], label: 'You'},
+    {data: [1300, 1818, 2400, 1992, 3800, 3890, 4200], label: 'Others'}
   ];
   // Common Chart Configuration
   public chartOptions: any = {
     responsive: true, // Resizes the chart canvas when its container does.
+    responsiveAnimationDuration: 250, // Duration in milliseconds it takes to animate to new size after a resize event.
+    maintainAspectRatio: true, // Maintain the original canvas aspect ratio (width / height) when resizing
+    scales: {
+      yAxes: [{
+        display: true,
+        gridLines: { 
+          display: true,
+          drawTicks: true },
+        ticks: { 
+          fontFamily: "'Arial'",
+          fontSize: '11',
+          fontWeight: 'lighter',
+          fontStyle: 'normal' }
+      }],
+      xAxes: [{
+        display: true,
+        gridLines: { 
+          display: true,
+          drawTicks: true },
+        ticks: { 
+          fontFamily: "'Arial'",
+          fontSize: '11',
+          fontWeight: 'lighter',
+          fontStyle: 'normal' }
+      }]
+    }
   };
+
+  // if true show legend below the chart, otherwise not be shown
+  public chartShowLegend: boolean = false;
+
+  constructor(){}
+
+  public ngOnInit(){
+    console.log(this.owChart);
+  }
 
   // events
   public chartClicked(e: any): void {
