fb464a41852c5a27f81546e799598aa9d6023524
Leon Lamle <llamle@gmail.com>
2017-05-22 12:19:31 -0400

    added data to platform-region

--- client/src/app/pages/profile/profile-header/profile-header.component.ts
+++ client/src/app/pages/profile/profile-header/profile-header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, Input } from '@angular/core';
+import { Player, SnapshotStats, HeroSnapshotStats, CombatLifetimeStats, MatchAwardsStats } from '../../../models';
 
 @Component({
   selector: 'ow-profile-header',
   templateUrl: 'profile-header.component.html',
   styleUrls: ['profile-header.component.scss']
 })
-export class ProfileHeaderComponent {
 
-  constructor() { }
+export class ProfileHeaderComponent implements AfterContentInit {
+  @Input() player: Player;
+  snapshotStats: SnapshotStats;
+  allHeroSnapshotStats: HeroSnapshotStats;
+  combatLifetimeStats: CombatLifetimeStats;
+  matchAwardsStats: MatchAwardsStats;
 
+  constructor() {}
+
+  ngAfterContentInit() {
+    this.snapshotStats = this.player.snapshotStatistics[this.player.snapshotStatistics.length - 1];
+    this.allHeroSnapshotStats = this.snapshotStats.allHeroesSnapshotStatistic;
+    this.combatLifetimeStats = this.allHeroSnapshotStats.combatLifetimeStatistic;
+    this.matchAwardsStats = this.allHeroSnapshotStats.matchAwardsStatistic;
+  }
 }
--- client/src/app/shared/platform-region/platform-region.component.ts
+++ client/src/app/shared/platform-region/platform-region.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { AfterContentInit, Component, Input } from '@angular/core';
+import { Player, SnapshotStats, HeroSnapshotStats, CombatLifetimeStats, MatchAwardsStats } from '../../models';
 @Component({
   selector: 'ow-platform-region',
   templateUrl: 'platform-region.component.html',
   styleUrls: ['platform-region.component.scss']
 })
-export class PlatformRegionComponent {
+export class PlatformRegionComponent implements AfterContentInit {
+  @Input() player: Player;
+  snapshotStats: SnapshotStats;
+  allHeroSnapshotStats: HeroSnapshotStats;
+  combatLifetimeStats: CombatLifetimeStats;
+  matchAwardsStats: MatchAwardsStats;
 
-  constructor() { }
+  constructor() {}
 
+  ngAfterContentInit() {
+    this.snapshotStats = this.player.snapshotStatistics[this.player.snapshotStatistics.length - 1];
+    this.allHeroSnapshotStats = this.snapshotStats.allHeroesSnapshotStatistic;
+    this.combatLifetimeStats = this.allHeroSnapshotStats.combatLifetimeStatistic;
+    this.matchAwardsStats = this.allHeroSnapshotStats.matchAwardsStatistic;
+  }
 }
