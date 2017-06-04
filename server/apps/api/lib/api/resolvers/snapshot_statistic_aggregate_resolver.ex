4f39a747c15380b540f6b30d5f59cf6e00da7db5
MikaAK <mika@kalathil.me>
2017-06-04 02:49:09 -0700

    fix: snaphsot statistics aggregate

--- server/apps/api/lib/api/resolvers/snapshot_statistic_aggregate_resolver.ex
+++ server/apps/api/lib/api/resolvers/snapshot_statistic_aggregate_resolver.ex
@@ -1,4 +1,4 @@
-defmodule Api.SnapshotStatisticAggregateResolver do
+defmodule Api.SnapshotStatisticsAggregateResolver do
   alias Models.Statistics.Snapshots.LatestSnapshotStatistic
 
   def aggregate(%{is_competitive: competitive}, _info), do: {:ok, LatestSnapshotStatistic.aggregate(competitive?: competitive)}
