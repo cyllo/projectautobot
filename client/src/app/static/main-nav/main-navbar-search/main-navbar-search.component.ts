5990e44d6f9cd1f4d93c49036b03dddd537fe3cb
cyllo <kurthogarth@gmail.com>
2017-06-23 15:55:21 -0700

    WIP: main nav styling

---
+++ client/src/app/static/main-nav/main-navbar-search/main-navbar-search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ow-main-navbar-search',
+  templateUrl: 'main-navbar-search.component.html',
+  styleUrls: ['main-navbar-search.component.scss']
+})
+
+export class MainNavbarSearchComponent {
+
+  constructor() {}
+
+}
