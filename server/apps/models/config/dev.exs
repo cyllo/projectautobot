c084fe405037caec583475bba7ab49a226accaf5
Nick Acker <acker2@me.com>
2017-06-27 12:56:38 -0700

    Fix dev postgres config

--- server/apps/models/config/dev.exs
+++ server/apps/models/config/dev.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :models, Models.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "stop_the_payload_dev",
-  username: "kurthogarth",
+  username: "postgres",
   hostname: "localhost",
   pool_size: 10
