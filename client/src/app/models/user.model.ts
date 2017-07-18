498edac6badf36cb665ca68e3c71d3413d4c712d
MikaAK <mika@kalathil.me>
2017-07-18 01:15:38 -0700

    fix: get rid of connectToBattleNet mutation in signup flow

--- client/src/app/models/user.model.ts
+++ client/src/app/models/user.model.ts
@@ -2,6 +2,8 @@ export interface User {
   id: number;
   email: string;
   displayName: string;
+  battleNetId: number;
+  battleNetTag: string;
   password?: string;
   insertedAt?: Date;
   updatedAt?: Date;
--- client/src/app/services/queries/create-user.mutation.ts
+++ client/src/app/services/queries/create-user.mutation.ts
@@ -1,11 +1,13 @@
 import gql from 'graphql-tag';
 
 export const CreateUserMutation = gql`
-mutation CreateUser($password: String!, $displayName: String!, $email: String!) {
-  createUser(password: $password, displayName: $displayName, email: $email) {
+mutation CreateUser($password: String!, $displayName: String!, $email: String!, $clientAuthToken: String) {
+  createUser(password: $password, displayName: $displayName, email: $email, clientAuthToken: $clientAuthToken) {
     id
     email
     displayName
+    battleNetTag
+    battleNetId
   }
 }
 `;
