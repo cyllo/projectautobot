b6f9dc628216bd642ee50c20235d0f5ec17c69f6
cyllo <kurthogarth@gmail.com>
2017-07-22 02:01:35 -0400

    feat: load fonts asynchronously using webfontloader

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { polyfillLoader } from 'polyfill-io-feature-detection';
+import * as WebFont from 'webfontloader';
 
 // depending on the env mode, enable prod mode or add debugging modules
 if (process.env.ENV === 'build') {
@@ -10,6 +11,11 @@ if (process.env.ENV === 'build') {
 
 function App () {
   // your app code here
+  WebFont.load({
+    google: {
+      families: ['Roboto']
+    }
+  });
   return platformBrowserDynamic().bootstrapModule(AppModule);
 }
 
