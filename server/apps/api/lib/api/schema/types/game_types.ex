8bc8f08f71af1d4156b03ed2712ba0dcd1051a2c
MikaAK <mika@kalathil.me>
2017-08-10 16:59:50 -0700

    fix: remove old stat fields from gamerTag

--- server/apps/api/lib/api/schema/types/game_types.ex
+++ server/apps/api/lib/api/schema/types/game_types.ex
@@ -8,20 +8,12 @@ defmodule Api.Schema.GameTypes do
     field :id, :integer
 
     field :tag, :string
+
     field :user_id, :integer
     field :overwatch_name, :string
-    field :portrait_url, :string
-    field :total_games_won, :integer
-
-    field :competitive_level, :integer
-    field :competitive_rank_url, :string
 
     field :region, :string
     field :platform, :string
-
-    field :level, :integer
-    field :level_url, :string
-    field :rank_url, :string
 
     timestamp_types
 
