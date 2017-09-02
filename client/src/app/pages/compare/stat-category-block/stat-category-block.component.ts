a99c7bd24ec73b1eff09c378bdd19d95e5959f95
Kurt Hogarth <kurthogarth@gmail.com>
2017-09-01 21:12:43 -0700

    chore(compare-page): updated UX to flex-layout (#89)

--- client/src/app/pages/compare/stat-category-block/stat-category-block.component.ts
+++ client/src/app/pages/compare/stat-category-block/stat-category-block.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatCategoryBlockComponent implements OnInit {
 
+  statBlocks = [
+    {
+      name: 'Combat',
+      stats: [
+        {
+          name: 'Eliminations',
+          value: 'eliminations'
+        },
+        {
+          name: 'Accuracy',
+          value: 'accuracy'
+        },
+        {
+          name: 'Damage Done',
+          value: 'damageDone'
+        },
+        {
+          name: 'Healing Done',
+          value: 'healingDone'
+        },
+        {
+          name: 'Damage Blocked',
+          value: 'damageBlocked'
+        },
+        {
+          name: 'Critical Hits',
+          value: 'criticalHits'
+        },
+        {
+          name: 'Objective Kills',
+          value: 'objectiveKills'
+        }
+      ]
+    },
+    {
+      name: 'Game',
+      stats: [
+        {
+          name: 'Gold Medals',
+          value: 'goldMedals'
+        }
+      ]
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
