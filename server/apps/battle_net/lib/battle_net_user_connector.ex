5d1d16ad06ce1cb70c9e527489b95718c64f889c
MikaAK <mika@kalathil.me>
2017-09-05 15:35:35 -0700

    feat: Set first gamer_tag to primary_gamer_tag on first scrape

--- server/apps/battle_net/lib/battle_net_user_connector.ex
+++ server/apps/battle_net/lib/battle_net_user_connector.ex
@@ -1,4 +1,8 @@
 defmodule BattleNet.UserConnector do
+  import Logger, only: [info: 1]
+
+  alias Models.Game
+
   @battle_net_connected_platforms "https://playoverwatch.com/en-us/career/get-platforms/"
 
   def connect_gamer_tags_to_user(user, gamer_tags) do
@@ -6,7 +10,12 @@ defmodule BattleNet.UserConnector do
       |> Enum.map(&Map.merge(&1, %{user_id: user.id}))
       |> Enum.map(&find_or_create_gamer_tag(&1, user.id))
 
-    {:ok, tags}
+
+    info "Connecting tags to #{user.display_name}: #{inspect Enum.map(tags, &(&1.id))}"
+
+    with {:ok, user} <- Models.Accounts.update_user(user, %{primary_gamer_tag_id: get_first_gamer_tag_id(tags)}) do
+      {:ok, tags}
+    end
   end
 
   def find_owned_gamer_tags(battle_net_id) do
@@ -25,11 +34,11 @@ defmodule BattleNet.UserConnector do
   end
 
   defp find_or_create_gamer_tag(gamer_tag, user_id) do
-    with {:ok, gamer_tag} <- Models.Game.find_gamer_tag(gamer_tag) do
+    with {:ok, gamer_tag} <- Game.find_gamer_tag(gamer_tag) do
       gamer_tag
     else
       _ ->
-        with {:ok, gamer_tag} <- gamer_tag |> Map.put(:user_id, user_id) |> Models.Game.create_gamer_tag do
+        with {:ok, gamer_tag} <- gamer_tag |> Map.put(:user_id, user_id) |> Game.create_gamer_tag do
           gamer_tag
         end
     end
@@ -38,4 +47,5 @@ defmodule BattleNet.UserConnector do
   defp get_gamer_tag(%{"careerLink" => link}), do: Regex.run(~r/[^\/]+$/, link) |> List.first
   defp get_region_param(%{"region" => "global"}), do: ""
   defp get_region_param(%{"region" => region}), do: region
+  defp get_first_gamer_tag_id(tags), do: tags |> List.first |> Map.get(:id)
 end
