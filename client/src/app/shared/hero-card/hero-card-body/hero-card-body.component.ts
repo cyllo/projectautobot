5154a503e4a5d242322ab2a808bc39858e7d7463
cyllo <kurthogarth@gmail.com>
2017-09-06 21:41:14 -0700

    added hero cards

--- client/src/app/shared/hero-card/hero-card-body/hero-card-body.component.ts
+++ client/src/app/shared/hero-card/hero-card-body/hero-card-body.component.ts
@@ -8,6 +8,61 @@ import { Component } from '@angular/core';
 
 export class HeroCardBodyComponent {
 
+  stats = [
+    {
+      name: 'Kills',
+      value: '0.25/min'
+    },
+    {
+      name: 'Assists',
+      value: '0.25/min'
+    },
+    {
+      name: 'Deaths',
+      value: '0.25/min'
+    },
+    {
+      name: 'K/D Ratio',
+      value: '0.25/min'
+    },
+    {
+      name: 'Accuracy',
+      value: '0.25/min'
+    },
+    {
+      name: 'Hero Damage',
+      value: '0.25/min'
+    },
+    {
+      name: 'Blocked',
+      value: '0.25/min'
+    },
+    {
+      name: 'Healing',
+      value: '0.25/min'
+    },
+    {
+      name: 'Crits',
+      value: '0.25/min'
+    },
+    {
+      name: 'Obj. Kills',
+      value: '0.25/min'
+    },
+    {
+      name: 'Obj. Time',
+      value: '0.25/min'
+    },
+    {
+      name: 'Ult Hold Time',
+      value: '0.25/mpg'
+    },
+    {
+      name: 'Ults Per Game',
+      value: '0.25/min'
+    }
+  ];
+
   constructor() {}
 
 }
