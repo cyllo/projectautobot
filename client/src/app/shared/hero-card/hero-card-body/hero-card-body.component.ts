6ad3606542f924acd579cd1b8f78f697e86d5eba
cyllo <kurthogarth@gmail.com>
2017-09-06 17:46:28 -0700

    cleanup

--- client/src/app/shared/hero-card/hero-card-body/hero-card-body.component.ts
+++ client/src/app/shared/hero-card/hero-card-body/hero-card-body.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ 'hero-card-body.component.scss' ]
 })
 
-export class HeroCardBodyComponent implements OnInit {
+export class HeroCardBodyComponent {
 
   constructor() {}
 
