497b4903dfd9245a9a04c086e3fe900b1b19d69b
BobbieBarker <sirkemera@gmail.com>
2017-09-10 11:53:46 -0700

    feat(profile page): made all over view components react to comp/qp toggle (#102)

--- client/src/app/pages/profile/overview/overview.component.ts
+++ client/src/app/pages/profile/overview/overview.component.ts
@@ -34,49 +34,34 @@ import { Observable } from 'rxjs/Observable';
 
 export class ProfileOverviewComponent implements OnInit {
   @Input() profile: GamerTag;
-  @Input() modeIndicator: {enabled: boolean, mode: string };
-  lifeTimeStats: CombatLifetimeStats;
-  gameHistory: GameHistoryStats;
-  heroes: any;
-  heroStatsByRole: any;
-  totalGameAverage: any;
-  matchAwards: any;
+  @Input() modeIndicator: Observable<{enabled: boolean, mode: string }>;
+  lifeTimeStats: Observable<CombatLifetimeStats>;
+  gameHistory: Observable<GameHistoryStats>;
+  heroStatsByRole: Observable<any>;
+  totalGameAverage: Observable<any>;
+  matchAwards: Observable<any>;
   skillRatingTrend: Observable<any[]>;
-  mostPlayedHeroes: any;
-  recentSnapshots: any[];
-  heroStatistics: any[];
+  mostPlayedHeroes: Observable<any[]>;
+  recentSnapshots: Observable<any[]>;
+  heroStatistics: Observable<any[]>;
 
   constructor(private gamerTagService: GamerTagService) {}
 
   ngOnInit() {
-    this.lifeTimeStats = <CombatLifetimeStats>path([
-      this.modeIndicator.mode,
-      'heroesTotalSnapshotStatistic',
-      'combatLifetimeStatistic'
-    ], this.profile);
+    this.lifeTimeStats = this.modeIndicator
+    .map(({ mode }) => path([mode, 'heroesTotalSnapshotStatistic', 'combatLifetimeStatistic'], this.profile));
 
-    this.gameHistory = <GameHistoryStats>path([
-      this.modeIndicator.mode,
-      'heroesTotalSnapshotStatistic',
-      'gameHistoryStatistic'
-    ], this.profile);
+    this.gameHistory = this.modeIndicator
+    .map(({ mode }) => path([mode, 'heroesTotalSnapshotStatistic', 'gameHistoryStatistic'], this.profile));
 
-    this.totalGameAverage = path([
-      this.modeIndicator.mode,
-      'heroesTotalSnapshotStatistic',
-      'gameAverageStatistic'
-    ], this.profile);
+    this.totalGameAverage = this.modeIndicator
+    .map(({ mode }) => path([mode, 'heroesTotalSnapshotStatistic', 'gameAverageStatistic'], this.profile));
 
-    this.matchAwards = path([
-      this.modeIndicator.mode,
-      'heroesTotalSnapshotStatistic',
-      'matchAwardsStatistic'
-    ], this.profile);
+    this.matchAwards = this.modeIndicator
+    .map(({ mode }) => path([mode, 'heroesTotalSnapshotStatistic', 'matchAwardsStatistic'], this.profile));
 
-    this.heroStatistics = <any>path([
-      this.modeIndicator.mode,
-      'heroSnapshotStatistics'
-    ], this.profile);
+    this.heroStatistics = this.modeIndicator
+    .map(({ mode }) => path([mode, 'heroSnapshotStatistics'], this.profile));
 
     const buildStatsByRole = heroes => {
       const kills = reduce((accum, hero) => add(accum, pathOr(0, ['combatLifetimeStatistic', 'eliminations'], hero)), 0, heroes);
@@ -86,30 +71,37 @@ export class ProfileOverviewComponent implements OnInit {
       return { kills, deaths, wins, timePlayed, KDratio: divide(kills, deaths).toFixed(2), name: path(['hero', 'role'], head(heroes)) };
     };
 
-    this.heroes = groupBy(path<string>(['hero', 'role']), path<any[]>([this.modeIndicator.mode, 'heroSnapshotStatistics'], this.profile));
-
-    this.heroStatsByRole = values(map(buildStatsByRole, this.heroes));
-
+    this.heroStatsByRole = this.modeIndicator
+    .map(({ mode }) => {
+      const heroes = groupBy(path<string>(['hero', 'role']), path<any[]>([mode, 'heroSnapshotStatistics'], this.profile));
+      return values(map(buildStatsByRole, heroes));
+    });
 
     this.skillRatingTrend = this.gamerTagService.skillRatingTrend(this.profile.id);
 
-    const gameTypeHeroes = path([this.modeIndicator.mode, 'heroSnapshotStatistics']);
-    const heroTimePlayed = path(['gameHistoryStatistic', 'timePlayed']);
-    const timePlayedComparator = comparator(useWith(gt, [heroTimePlayed, heroTimePlayed]));
+    this.mostPlayedHeroes = this.modeIndicator
+    .map(({ mode }) => {
+      const gameTypeHeroes = path([mode, 'heroSnapshotStatistics']);
+      const heroTimePlayed = path(['gameHistoryStatistic', 'timePlayed']);
+      const timePlayedComparator = comparator(useWith(gt, [heroTimePlayed, heroTimePlayed]));
 
-    const sortHeroesByMostPlayed = compose(take(4), sort(timePlayedComparator), gameTypeHeroes);
+      const sortHeroesByMostPlayed = compose(take(4), sort(timePlayedComparator), gameTypeHeroes);
+      return sortHeroesByMostPlayed(this.profile);
+    });
 
-    this.mostPlayedHeroes = sortHeroesByMostPlayed(this.profile);
 
-    const selectHeroesSnapshot = converge(merge, [
-      pick(['insertedAt', 'profileSnapshotStatistic']),
-      applySpec({
-        heroesTotalSnapshotStatistic: prop(`${toLower(this.modeIndicator.mode)}HeroesTotalSnapshotStatistic`),
-        heroSnapshotStatistic: prop(`${toLower(this.modeIndicator.mode)}HeroSnapshotStatistics`)
-      })
-    ]);
+    this.recentSnapshots = this.modeIndicator
+    .map(({ mode }) => {
+      const selectHeroesSnapshot = converge(merge, [
+        pick(['insertedAt', 'profileSnapshotStatistic']),
+        applySpec({
+          heroesTotalSnapshotStatistic: prop(`${toLower(mode)}HeroesTotalSnapshotStatistic`),
+          heroSnapshotStatistic: prop(`${toLower(mode)}HeroSnapshotStatistics`)
+        })
+      ]);
 
-    const recentStats = compose(map(selectHeroesSnapshot), prop('snapshotStatistics'));
-    this.recentSnapshots = recentStats(this.profile);
+      const recentStats = compose(map(selectHeroesSnapshot), prop('snapshotStatistics'));
+      return recentStats(this.profile);
+    });
   }
 }
