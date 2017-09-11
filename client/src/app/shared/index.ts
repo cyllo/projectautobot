a171a499e560feebbc2a3d9bdf3d8b5a5ff091d7
cyllo <kurthogarth@gmail.com>
2017-09-11 14:25:30 -0700

    chore(news-posts): cleaned up news page and news posts on home

--- client/src/app/shared/index.ts
+++ client/src/app/shared/index.ts
@@ -15,7 +15,6 @@ export * from './hero-skill-video';
 export * from './hero-synergy-counter';
 export * from './hero-catalog';
 export * from './hero-portrait';
-export * from './top-news';
 export * from './profile-checkbox';
 export * from './hero-checkbox';
 export * from './quick-comp-toggle';
